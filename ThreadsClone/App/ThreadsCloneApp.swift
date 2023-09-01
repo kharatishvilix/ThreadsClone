4fec6925231c50fd8eb5959fb239d251607d2b71
Kharatishvilix <Kharatishvililuka@gmail.com>
2023-09-01 19:22:59 +0400

    initial commit

---
+++ ThreadsClone/App/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Luka  Kharatishvili on 01.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
